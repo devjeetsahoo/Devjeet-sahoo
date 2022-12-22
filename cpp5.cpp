eb05db60bd0eba4d4adffaf06aac4115efe2760c
Devjeet Sahoo <118748163+devjeetsahoo@users.noreply.github.com>
2022-12-22 15:51:30 +0530

    Add files via upload

---
+++ cpp5.cpp
@@ -0,0 +1,38 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     int i = 1;
+//     int sum = 0;
+//     while (i <= a)
+//     {
+//         sum = sum + 1;
+//         i = i + 1;
+//         cout << "The Sum is " << sum<<endl;
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=2;
+    while(i<n){
+        if(n%i==0)
+        {
+            cout<<" The no. is not prime for "<<endl;
+
+        }
+        else {
+            cout<<"the no. is prime for "<<endl;
+        }
+        i=i+1;
+
+    }
+    
+}
+
