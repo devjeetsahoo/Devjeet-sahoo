9bcb68c2844d79e59294661dbb50b6fff79fd943
Devjeet Sahoo <118748163+devjeetsahoo@users.noreply.github.com>
2022-12-22 15:52:16 +0530

    Update cpp5.cpp

--- cpp5.cpp
+++ cpp5.cpp
@@ -1,18 +1,18 @@
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int a;
-//     cin >> a;
-//     int i = 1;
-//     int sum = 0;
-//     while (i <= a)
-//     {
-//         sum = sum + 1;
-//         i = i + 1;
-//         cout << "The Sum is " << sum<<endl;
-//     }
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    int i = 1;
+    int sum = 0;
+    while (i <= a)
+    {
+        sum = sum + 1;
+        i = i + 1;
+        cout << "The Sum is " << sum<<endl;
+    }
+}
 
 #include <iostream>
 using namespace std;
